17674a24890dc75d11434012e29df95bc95b2a31
Vikrant Khedkar <vikrantkhedkar2720@gmail.com>
2021-08-01 14:22:50 +0530

    Added views in shop app

--- buyit/shop/urls.py
+++ buyit/shop/urls.py
@@ -19,6 +19,11 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path("",views.index,name="Shophome")
-
+    path("",views.index,name="Shophome"),
+    path("about/",views.about,name="AboutUs"),
+    path("contact/",views.contact,name="ContactUs"),
+    path("tracker/",views.tracker,name="TrackingStatus"),
+    path("search/",views.search,name="Search"),
+    path("search/",views.productView,name="ProductView"),
+    path("search/",views.checkout,name="Checkout"),
 ]
--- buyit/shop/views.py
+++ buyit/shop/views.py
@@ -6,3 +6,27 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'shop/index.html')
+
+
+def about(request):
+    return HttpResponse("In about")
+
+
+def contact(request):
+    return HttpResponse("In contact")
+
+
+def tracker(request):
+    return HttpResponse("In tracker")
+
+
+def search(request):
+    return HttpResponse("In search")
+
+
+def productView(request):
+    return HttpResponse("In productView")
+
+
+def checkout(request):
+    return HttpResponse("In checkout")
